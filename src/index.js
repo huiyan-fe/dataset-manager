c63779f0edb120c8ae8e35161183ebd99c300319
Junior2Ran <hdr01@126.com>
2018-10-08 17:18:28 +0800

    add some function undone

--- src/index.js
+++ src/index.js
@@ -27,18 +27,13 @@ export default class DataSetManager {
         return this;
     }
 
-    geoPoint(lngColumnName, latColumnName) {
-        let data = this.data.data;
-        for (let i = 0; i < data.length; i++) {
-            data[i].geometry = {
-                type: 'Point',
-                coordinates: [data[i][lngColumnName], data[i][latColumnName]]
-            };
-        }
-        return this;
-    }
-
-    geoPointWithCount(lngColumnName, latColumnName, countColumnName) {
+    /**
+     * 解析点坐标数据
+     * @param {string} lngColumnName 点坐标经度列名
+     * @param {string} latColumnName 点坐标纬度列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoPoint(lngColumnName, latColumnName, countColumnName) {
         let data = this.data.data;
         for (let i = 0; i < data.length; i++) {
             data[i].geometry = {
@@ -50,31 +45,13 @@ export default class DataSetManager {
         return this;
     }
 
-    geoAddress(columnName, callback) {
+    /**
+     * 解析点位置数据
+     * @param {string} addrColumnName 地址列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoAddress(addrColumnName, countColumnName, callback) {
         let data = this.data.data;
-        console.log('geoAddress');
-        batchGeoCoding(data.map((item) => {
-            return {
-                name: item[columnName]
-            };
-        }), (rs) => {
-            for (let i = 0; i < data.length; i++) {
-                data[i].geocoding = rs[i];
-                if (data[i].geocoding && data[i].geocoding.location) {
-                    let location = data[i].geocoding.location;
-                    data[i].geometry = {
-                        type: 'Point',
-                        coordinates: [location.lng, location.lat]
-                    };
-                }
-            }
-            callback && callback(data);
-        });
-    }
-
-    geoAddressWithCount(addrColumnName, countColumnName, callback) {
-        let data = this.data.data;
-        console.log('geoAddress');
         batchGeoCoding(data.map((item) => {
             return {
                 name: item[addrColumnName],
@@ -94,6 +71,66 @@ export default class DataSetManager {
             }
             callback && callback(data);
         });
+    }
+
+    /**
+     * 解析线坐标数据
+     * @param {string} lngStartColumnName 起点坐标经度列名
+     * @param {string} latStartColumnName 起点坐标纬度列名
+     * @param {string} lngEndColumnName 终点坐标经度列名
+     * @param {string} latEndColumnName 终点坐标纬度列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoLine(lngStartColumnName, latStartColumnName, lngEndColumnName, latEndColumnName, countColumnName) {
+        let data = this.data.data;
+        for (let i = 0; i < data.length; i++) {
+            data[i].geometry = {
+                type: 'LineString',
+                coordinates: [
+                    [data[i][lngStartColumnName], data[i][latStartColumnName]],
+                    [data[i][lngEndColumnName], data[i][latEndColumnName]],
+                ]
+            };
+            data[i].count = parseFloat(data[i][countColumnName]) || 1;
+        }
+        return this;
+    }
+
+    /**
+     * 解析线坐标串数据
+     * @param {string} positionColumnName 坐标字符串列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoLineString(positionColumnName, countColumnName) {
+        
+    }
+
+    /**
+     * 解析线位置数据
+     * @param {string} startColumnName 起点位置列名
+     * @param {string} endColumnName 终点位置列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoRoute(startColumnName, endColumnName, countColumnName, callback) {
+
+    }
+
+    /**
+     * 解析面坐标串数据
+     * @param {string} positionColumnName 坐标字符串列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoPolygon(positionColumnName, countColumnName) {
+
+    }
+
+    /**
+     * 解析面位置数据
+     * @param {string} areaColumnName 面位置列名
+     * @param {string} countColumnName 权重列名
+     */
+    geoArea(areaColumnName, countColumnName, callback) {
+
     }
 
     /**
